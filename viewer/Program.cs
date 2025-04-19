84ecdf4cabbeee06a9c40bb2676517873dd6e18d
zygisau1 <zygimantasaugunas@gmail.com>
2025-04-19 22:24:42 +0300

    FIX: Enable not only in dev.

--- viewer/Program.cs
+++ viewer/Program.cs
@@ -29,15 +29,12 @@ builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = "swagger"; // Serve UI at /swagger
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = "swagger"; // Serve UI at /swagger
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
